b1e5c80e114debe1d7007856e09efa1dfb445d67
Virginie <vbouvarel@lilo.org>
2021-08-05 10:50:32 +0200

    ajout accordeon faq

--- projet-dopetheme/js/data/data_home.js
+++ projet-dopetheme/js/data/data_home.js
@@ -43,20 +43,5 @@ let data = {
             }
         ]
     },
-    faq: {
-        questions: [
-            {
-                sentence: 'Started several mistake joy say painful removed reached end',
-                answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            },
-            {
-                sentence: 'Although moreover mistaken kindness me feelings do be marianne?',
-                answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            },
-            {
-                sentence: 'By in no ecstatic wondered disposal my speaking?',
-                answer: 'Far curiosity incommode now led smallness allowance. Favour bed assure son things yet. She consisted consulted elsewhere happiness disposing household any old the.Widow downs you new shade drift hopes small.So otherwise commanded sweetness we improving.'
-            }
-        ]
-    }
+
 }
